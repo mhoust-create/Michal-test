a59650a9d909166a30401011656533888e645d31
mhoust-create <mhoust@gmail.com>
2026-06-10 21:37:10 +0000

    Deploying to gh-pages from @ mhoust-create/Michal-test@35a00bb23ff8cadd83e93ac7f42c7ca4ef0e506e 🚀

---
+++ supplements/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Michal-test/supplements/sw.js', { scope: '/Michal-test/supplements/' })})}
